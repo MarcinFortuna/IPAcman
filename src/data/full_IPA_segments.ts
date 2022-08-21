f514b5c17b58b7fa8e22ed03af5d96d8c4009263
Marcin Fortuna <marcin.fort@gmail.com>
2022-08-21 12:56:37 +0200

    all bilabial sounds in full IPA

--- src/data/full_IPA_segments.ts
+++ src/data/full_IPA_segments.ts
@@ -5,8 +5,68 @@ export const full_consonants_pulmonic = [
         "sampa": "p",
         "airstream": "pulmonic",
         "place": "bilabial",
-        "manner": "plosive"
-    }
+        "manner": "plosive",
+        "voicing": "voiceless",
+        "url": "https://en.wikipedia.org/wiki/Voiceless_bilabial_plosive",
+        "audio": "https://en.wikipedia.org/wiki/File:Voiceless_bilabial_plosive.ogg"
+    },
+
+    {
+        "ipa": "b",
+        "sampa": "b",
+        "airstream": "pulmonic",
+        "place": "bilabial",
+        "manner": "plosive",
+        "voicing": "voiced",
+        "url": "https://en.wikipedia.org/wiki/Voiced_bilabial_plosive",
+        "audio": "https://en.wikipedia.org/wiki/File:Voiced_bilabial_plosive.ogg"
+    },
+
+    {
+        "ipa": "m",
+        "sampa": "m",
+        "airstream": "pulmonic",
+        "place": "bilabial",
+        "manner": "nasal",
+        "voicing": "voiced",
+        "url": "https://en.wikipedia.org/wiki/Voiced_bilabial_nasal",
+        "audio": "https://en.wikipedia.org/wiki/File:Bilabial_nasal.ogg"
+    },
+
+    {
+        "ipa": "ʙ",
+        "sampa": "B\\",
+        "airstream": "pulmonic",
+        "place": "bilabial",
+        "manner": "trill",
+        "voicing": "voiced",
+        "url": "https://en.wikipedia.org/wiki/Voiced_bilabial_trill",
+        "audio": "https://en.wikipedia.org/wiki/File:Bilabial_trill.ogg"
+    },
+
+    {
+        "ipa": "ɸ",
+        "sampa": "p\\",
+        "airstream": "pulmonic",
+        "place": "bilabial",
+        "manner": "fricative",
+        "voicing": "voiceless",
+        "url": "https://en.wikipedia.org/wiki/Voiceless_bilabial_fricative",
+        "audio": "https://en.wikipedia.org/wiki/File:Voiceless_bilabial_fricative.ogg"
+    },
+
+    {
+        "ipa": "β",
+        "sampa": "B",
+        "airstream": "pulmonic",
+        "place": "bilabial",
+        "manner": "fricative",
+        "voicing": "voiced",
+        "url": "https://en.wikipedia.org/wiki/Voiced_bilabial_fricative",
+        "audio": "https://en.wikipedia.org/wiki/File:Voiced_bilabial_fricative.ogg"
+    },
+
+
 
 ]
 
