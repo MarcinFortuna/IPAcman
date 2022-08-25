4d86292ce09b45bcd225ea6d97fd6d7012babfc1
Marcin Fortuna <marcin.fort@gmail.com>
2022-08-25 14:07:04 +0200

    add coronal consonants

--- src/data/full_IPA_segments.ts
+++ src/data/full_IPA_segments.ts
@@ -119,6 +119,160 @@ export const full_consonants_pulmonic = [
         voicing: "voiced",
         url: "https://en.wikipedia.org/wiki/Voiced_labiodental_approximant",
         audio: "https://en.wikipedia.org/wiki/File:Labiodental_approximant.ogg"
+    },
+
+    {
+        ipa: "t",
+        sampa: "t",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "plosive",
+        voicing: "voiceless",
+        url: "https://en.wikipedia.org/wiki/Voiceless_dental_and_alveolar_plosives",
+        audio: "https://en.wikipedia.org/wiki/File:Voiceless_alveolar_plosive.ogg"
+    },
+
+    {
+        ipa: "d",
+        sampa: "d",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "plosive",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental_and_alveolar_plosives",
+        audio: "https://en.wikipedia.org/wiki/File:Voiced_alveolar_plosive.ogg"
+    },
+
+    {
+        ipa: "r",
+        sampa: "r",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "trill",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental,_alveolar_and_postalveolar_trills",
+        audio: "https://en.wikipedia.org/wiki/File:Alveolar_trill.ogg"
+    },
+
+    {
+        ipa: "ɾ",
+        sampa: "4",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "tap",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental_and_alveolar_taps_and_flaps",
+        audio: "https://en.wikipedia.org/wiki/File:Alveolar_tap.ogg"
+    },
+
+    {
+        ipa: "θ",
+        sampa: "T",
+        airstream: "pulmonic",
+        place: "dental",
+        manner: "fricative",
+        voicing: "voiceless",
+        url: "https://en.wikipedia.org/wiki/Voiceless_dental_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiceless_dental_fricative.ogg"
+    },
+
+    {
+        ipa: "ð",
+        sampa: "D",
+        airstream: "pulmonic",
+        place: "dental",
+        manner: "fricative",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiced_dental_fricative.ogg"
+    },
+
+    {
+        ipa: "s",
+        sampa: "s",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "fricative",
+        voicing: "voiceless",
+        url: "https://en.wikipedia.org/wiki/Voiceless_alveolar_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiceless_alveolar_sibilant.ogg"
+    },
+
+    {
+        ipa: "z",
+        sampa: "z",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "fricative",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_alveolar_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiced_alveolar_sibilant.ogg"
+    },
+
+    {
+        ipa: "ʃ",
+        sampa: "S",
+        airstream: "pulmonic",
+        place: "postalveolar",
+        manner: "fricative",
+        voicing: "voiceless",
+        url: "https://en.wikipedia.org/wiki/Voiceless_postalveolar_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiceless_palato-alveolar_sibilant.ogg"
+    },
+
+    {
+        ipa: "ʒ",
+        sampa: "Z",
+        airstream: "pulmonic",
+        place: "postalveolar",
+        manner: "fricative",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_postalveolar_fricative",
+        audio: "https://en.wikipedia.org/wiki/File:Voiced_palato-alveolar_sibilant.ogg"
+    },
+
+    {
+        ipa: "ɬ",
+        sampa: "K",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "lateral fricative",
+        voicing: "voiceless",
+        url: "https://en.wikipedia.org/wiki/Voiceless_dental_and_alveolar_lateral_fricatives",
+        audio: "https://en.wikipedia.org/wiki/File:Voiceless_alveolar_lateral_fricative.ogg"
+    },
+
+    {
+        ipa: "ɮ",
+        sampa: "K\\",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "lateral fricative",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental_and_alveolar_lateral_fricatives",
+        audio: "https://en.wikipedia.org/wiki/File:Voiced_alveolar_lateral_fricative.ogg"
+    },
+
+    {
+        ipa: "ɹ",
+        sampa: "r\\",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "approximant",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_alveolar_and_postalveolar_approximants",
+        audio: "https://en.wikipedia.org/wiki/File:Alveolar_approximant.ogg"
+    },
+
+    {
+        ipa: "l",
+        sampa: "l",
+        airstream: "pulmonic",
+        place: "alveolar",
+        manner: "lateral approximant",
+        voicing: "voiced",
+        url: "https://en.wikipedia.org/wiki/Voiced_dental,_alveolar_and_postalveolar_lateral_approximants",
+        audio: "https://en.wikipedia.org/wiki/File:Alveolar_lateral_approximant.ogg"
     }
 
 ]
